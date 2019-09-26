471e1213e74ef386c8fbe2e9a8d61a10c9e83e2a
Nathaniel <nathanielsenje@hotmail.com>
2019-09-26 14:22:49 +0300

    Initial commit

---
+++ src/components/layout/MainFooter/MainFooter.vue
@@ -0,0 +1,61 @@
+<template>
+  <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
+    <div>
+      <p class="text-center">Created by Nathaniel Senje</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+const defaultMenuItems = [
+  {
+    title: "Home",
+    to: "#"
+  },
+  {
+    title: "Services",
+    to: "#"
+  },
+  {
+    title: "About",
+    to: "#"
+  },
+  {
+    title: "Products",
+    to: "#"
+  },
+  {
+    title: "Blog",
+    to: "#"
+  }
+];
+
+export default {
+  name: "main-footer",
+  props: {
+    /**
+     * The footer menu items.
+     */
+    menuItems: {
+      type: Array,
+      default() {
+        return defaultMenuItems;
+      }
+    },
+    /**
+     * The copyright information.
+     */
+    copyright: {
+      type: String,
+      default: "Copyright © 2018 DesignRevision"
+    },
+    /**
+     * Whether the footer should be wrapped in a container, or not.
+     */
+    contained: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
